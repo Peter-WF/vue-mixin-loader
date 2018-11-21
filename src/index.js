6f22d1ae3ebb1a72b8f39e71bc2bee932fa64a77
wangfei37 <wangfei37@baidu.com>
2018-11-21 15:42:54 +0800

    feat: update inject less tag

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ module.exports = function (source) {
     this.cacheable();
 
     const config = loaderUtils.getOptions(this);
-    const compass = '@import "' + path.relative(path.dirname(this.resourcePath), config.mixinPath) + '";';
+    const compass = "@import '" + path.relative(path.dirname(this.resourcePath), config.mixinPath) + "';";
 
     return source.replace(/(<style.+lang=['"]less['"]\s?.*>)/g, `$1\n${compass}`);
   }
